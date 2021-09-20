9573801a4a02814cdd00f12ede28e98be00e079f
Alexander <alex030t@stud.kea.dk>
2021-09-20 13:30:45 +0200

    2nd commit

--- src/main/java/restdemodat20/demo/controller/StudentController.java
+++ src/main/java/restdemodat20/demo/controller/StudentController.java
@@ -1,6 +1,7 @@
 package restdemodat20.demo.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import restdemodat20.demo.model.Student;
 import restdemodat20.demo.repository.StudentRepository;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
@@ -24,9 +26,23 @@ public class StudentController {
 
     //HTTP GET (uri = /students)
     @GetMapping(value = "/students")
-    public ResponseEntity<List<Student>> findAll(){
+    public ResponseEntity<List<Student>> findAll() {
         List<Student> tmp = new ArrayList<>();
         studentRep.findAll().forEach(tmp::add);
+        //ResponseEntity builder
         return ResponseEntity.status(HttpStatus.OK).body(tmp);
     }
+
+
+    @GetMapping(value = "/students/{id}")
+    public ResponseEntity<Student> findSpecificStudent(@PathVariable("id") long id) {
+        Optional<Student> tmp = studentRep.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(tmp.get());
+    }
+
+    //Post create
+
+    //Put update
+
+    //Delete delete
 }
